48cff6ccacd5bb4765daa94663bea5f2017772e2
Marri Supriya <supriyayadavmarri@gmail.com>
2025-12-26 13:53:00 +0530

    Time: 612 ms (36.60%) | Memory: 20.2 MB (91.89%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -11,13 +11,12 @@ class Solution:
                 sum1=nums[i]+nums[l]+nums[r]
                 if sum1==0:
                     c.append([nums[i],nums[l],nums[r]])
-                    left_val, right_val = nums[l], nums[r]
                     l+=1
                     r-=1
-                    while l < r and nums[l] == left_val:
-                        l += 1
-                    while l < r and nums[r] == right_val:
-                        r -= 1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
                 elif sum1<0:
                     l+=1
                 else:
